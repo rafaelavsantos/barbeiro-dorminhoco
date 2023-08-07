be877471886903862b19e025461007601675c420
Rafaela Santos <rafaelasantosminios@gmail.com>
2023-08-07 17:29:36 -0300

    algoritmo com bibliotecas

--- barbeiro-dorminhoco.c
+++ barbeiro-dorminhoco.c
@@ -4,79 +4,87 @@
 #include "pthread.h"
 #include "semaphore.h"
 
-#define N_CHAIRS 5 /* número de cadeiras para os clientes aguardarem */
+#define N_CHAIRS 5 // número de cadeiras para os clientes aguardarem 
 #define N_CUSTOMERS 10
 
-typedef int semaphore; /* iniciando o semáforo */
-semaphore customers; /* número inicial de clientes à espera de atendimento */
-semaphore barbers; /* número inicial de barbeiros à espera de clientes */
-semaphore mutex; /* acesso para a exclusão mútua */
+sem_t customers; // contador de clientes esperando
+sem_t barbers; // contador de barbeiros esperando
+sem_t mutex; // mutex para controlar o acesso às variáveis compartilhadas
 
-int waiting = 0; /* número inicial de clientes que estão esperando */
-
+int waiting = 0; // contador de clientes na sala de espera
 /* Protótipos das funções */
-void *barber(void *arg);
+void barber();
 void *customer(void *arg);
 void cut_hair();
-void customer_arrived();
+void customer_arrived(int client);
 void get_haircut();
 void giveup_haircut();
 
-int main(){
-    sem_init((sem_t*)&customers, 1, N_CHAIRS);
-    sem_init((sem_t*)&barbers, 1, 1);
-    sem_init((sem_t*)&mutex, 1, 0);
-
-    pthread_t b, c;
+int main(){    
+    sem_init(&customers, 1, 0);
+    sem_init(&barbers, 1, 0);
+    sem_init(&mutex, 1, 1);
     
-    pthread_create(&b, NULL, (void *) barber, NULL); /* Criando um único barbeiro */
+    pthread_t barber_thread;
+    pthread_t customer_threads[N_CUSTOMERS];
 
-    while(1) {
-        pthread_create(&c, NULL, (void *) customer, NULL); /* Criação indefinida de cliente */
+    pthread_create(&barber_thread, NULL, (void *) barber, NULL); // Criando um único barbeiro 
+
+    int ids[N_CUSTOMERS];
+    for(int i=0;i < N_CUSTOMERS; i++){
+        ids[i] = i;
+        pthread_create(&customer_threads[i], NULL, (void *) customer, &ids[i]); // Criação indefinida de cliente
         sleep(1);
     }
-
+    for (int i = 0; i < N_CUSTOMERS; i++){
+       pthread_join(customer_threads[i], NULL);
+    }  
     return 0;
 }
 
-void *barber(void *arg) {
+void barber() {
     while(1) {
-       sem_wait(&customers); /* o barbeiro dorme se nº de clientes for 0 */
-       sem_wait(&mutex); /* obtém acesso a 'waiting' */
-       waiting = waiting - 1; /* descreve o contador de clientes aguardando */
-       sem_post(&barbers); /* o barbeiro está pronto para cortar o cabelo do cliente */
-       sem_post(&mutex); /* libera 'waiting' */
-       cut_hair(); /* corta o cabelo (fora da região crítica) */
-       giveup_haircut();
+       sem_wait(&customers); // o barbeiro dorme se nº de clientes for 0 
+       sem_wait(&mutex); // obtém acesso a 'waiting' 
+       waiting--; // descreve o contador de clientes aguardando 
+       sem_post(&barbers); // o barbeiro está pronto para cortar o cabelo do cliente 
+       sem_post(&mutex); // libera 'waiting' 
+       
+       cut_hair(); // corta o cabelo (fora da região crítica) 
     }
     pthread_exit(NULL);
 }
+
 void *customer(void *arg) { 
-    sem_wait(&mutex); /* entra na região crítica */
-    int id = *((int *)arg); 
-    sleep(id); /* Espaço entre chegada de clientes */
-    if(waiting < N_CHAIRS) { /* se houver cadeiras livres, entra no laço */
-       customer_arrived(); /* cliente chegou!*/
-       waiting = waiting + 1; /* incrementa o contador de clientes aguardando */
-       sem_post(&customers); /* acorda o barbeiro */
-       sem_post(&mutex); /* libera o acesso a 'waiting' */
-       sem_wait(&barbers); /* vai dormir se o número de barbeiros livres for 0 */
-       get_haircut(); /* sentado e sendo servido */
+    sem_wait(&mutex); // entra na região crítica 
+    int id = *((int *)arg);
+    sleep(id); // Espaço de tempo entre chegada de clientes
+
+    if(waiting < N_CHAIRS) { // se houver cadeiras livres, entra no laço 
+        customer_arrived(id); // cliente chegou!
+        waiting++; // incrementa o contador de clientes aguardando 
+        
+        sem_post(&customers); // acorda o barbeiro 
+        sem_post(&mutex); // libera o acesso a 'waiting' 
+        sem_wait(&barbers); // vai dormir se o número de barbeiros livres for 0 
+        get_haircut(); // sentado e sendo servido 
     } else {
-        sem_post(&mutex); /* a barbeira está cheia, não espere*/
+        sem_post(&mutex); // a barbearia está cheia, não espere
+        giveup_haircut();   
     }
     pthread_exit(NULL);
 }
+
 void cut_hair() {
-    printf("O Barbeiro está cortando o cabelo!\n");
+    printf("Barbeiro está cortando o cabelo do cliente!\n");
     sleep(3);
 }
-void customer_arrived() {
-    printf("O cliente chegou!\n");
+void customer_arrived(int client) {
+    printf("Cliente %d chegou para cortar o cabelo!\n", client);
 }
 void get_haircut() {
-    printf("O cliente está sendo servido!\n");
+    printf("Cliente está tendo o cabelo cortado!\n");
 }
 void giveup_haircut() {
-    printf("O cliente desistiu! O salão está muito cheio!\n");
+    printf("Cliente desistiu! O salão está muito cheio!\n");
 }
